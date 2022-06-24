11d52f891769d440d3336a9e36a709b3fc1409cb
sangmin <xectler@gmail.com>
2022-06-24 21:20:55 +0900

    update

--- src/pages/app/views/ProcessExepertPage.vue
+++ src/pages/app/views/ProcessExepertPage.vue
@@ -3,29 +3,34 @@
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th>아이디</th>
+                <th>교정 문장</th>
+                <th>수집 문장</th>
+                <th>띄어쓰기 오류</th>
+                <th>문장부호 오류</th>
+                <th>기타 오류1</th>
+                <th>기타 오류2</th>
+                <th>적용</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
+            <tr v-for="(item, index) in sentenceArray">
+                <th>{{ item.id }}</th>
+                <td>{{ item.sentence }}</td>
+                <td>{{ item.cor_sentence }}</td>
+                <td class="input"><input type="checkbox" :checked="item.error_type_spac? true : false"  @change="e => {
+                changeHandler(e, sentenceArray[index])
+                } "></td>
+                <td class="input"><input type="checkbox" :checked="item.error_type_mark? true : false" @change="e => {
+                changeHandler(e, sentenceArray[index])
+                } "></td>
+                <td class="input"><input type="checkbox" :checked="item.error_type_etc? true : false"  @change="e => {
+                changeHandler(e, sentenceArray[index])
+                } "></td>
+                <td class="input"><input type="checkbox" :checked="item.error_type_etc? true : false"  @change="e => {
+                changeHandler(e, sentenceArray[index])
+                } "></td>
+                <td><button>적용하기</button></td>
             </tr>
         </tbody>
     </table>
@@ -33,16 +38,19 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 import { getProcessExpertData } from '@/api/work'
 
 export default {
     setup(){
+        
+        const sentenceArray = ref([])
 
         const getData = () => {
             const res = getProcessExpertData()
 
             res.then(result => {
-                console.log(result)
+                sentenceArray.value = result.data.data 
             }).catch(error => {
                 console.log(error)
             })
@@ -50,13 +58,21 @@ export default {
 
         getData()
 
-        return {
+        const changeHandler = (e, obj) => {
+            console.log(e.target.checked)
+            console.log(obj)
+        }
 
+        return {
+            sentenceArray,
+            changeHandler
         }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+.input {
+    text-align: center;
+}
 </style>
